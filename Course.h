5e405d1b13da978ba70b70d31984d402bed30edc
oracletrion <tommy26tran@gmail.com>
2015-10-22 18:18:26 -0700

    Update Course.h

--- Course.h
+++ Course.h
@@ -12,28 +12,28 @@ class Course
 public:
 
 	// default constructor
-	
+	Course();
 
 	// setCourseInfo
-
+	void setCourseInfo(const string& newCourseN, const string& newCourseNum, char& newGrade, int& newCredits);
 
 	// printCourseInfo
-
+	void printCourseInfo(bool isgrade) const; 
 
 	// getCourseCredits
-
+	int getCourseCredits() const;
 
 	// getCourseNumber
-
+	string getCourseNumber() const;
 
 	// getCourseName
-
+	string getCourseName() const;
 
 	// getCourseGrade
-
+	char getCourseGrade() const;
 
 	// destructor
-
+	~Course(); 
 
 private:
     string courseName; 	
